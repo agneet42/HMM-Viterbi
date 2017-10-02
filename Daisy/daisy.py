761f63bfcd1090bf19ce59a5ae7bb3ca9ce872ea
Agneet Chatterjee <agneet257@gmail.com>
2017-10-03 01:39:45 +0530

    Update daisy.py

--- Daisy/daisy.py
+++ Daisy/daisy.py
@@ -1,16 +1,26 @@
 import skimage
 from skimage.feature import daisy
-
 from skimage import data
 import matplotlib.pyplot as plt
+import os
+import numpy as np
+import csv
 
+file = csv.writer(open("featurefile1.csv",'a'))
 
-img = skimage.io.imread('resized.bmp')
-descs, descs_img = daisy(img, visualize=True)
-print(descs.shape)
-fig, ax = plt.subplots()
-ax.axis('off')
-ax.imshow(descs_img)
-descs_num = descs.shape[0] * descs.shape[1]
-ax.set_title('%i DAISY descriptors extracted:' % descs_num)
-plt.show()
+s = os.listdir()
+for i in s:
+	if(i[0]=='r'):
+		img = skimage.io.imread(i)
+		descs, descs_img = daisy(img, visualize=True)
+		arr_to_write = []
+		for row in range(0,5):
+			for col in range(0,5):
+				arr = descs[row][col]
+				arr1,extra = np.histogram(arr,bins =10)
+				# print(type(arr1))
+				arr_to_write = np.append(arr_to_write,arr1)
+		arr_to_write = arr_to_write.astype(np.int64)
+		arr_to_write = arr_to_write.tolist()
+		arr_to_write.append('A49') # to be changed as per class name
+		file.writerow(arr_to_write)
